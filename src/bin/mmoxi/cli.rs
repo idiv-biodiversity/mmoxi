b59923756746fc09254baf664eb3e85a68603677
Christian Krause <christian.krause@idiv.de>
2022-03-01 08:20:27 +0100

    adds the cli

---
+++ src/bin/mmoxi/cli.rs
@@ -0,0 +1,158 @@
+use clap::{crate_name, crate_version};
+use clap::{Arg, ArgMatches, Command};
+
+pub fn args() -> ArgMatches {
+    build().get_matches()
+}
+
+pub fn build() -> Command<'static> {
+    let fs = Arg::new("filesystem")
+        .takes_value(true)
+        .required(true)
+        .help("file system");
+
+    let pool = Arg::new("pool")
+        .takes_value(true)
+        .required(true)
+        .help("pool name");
+
+    let pool_percent = Command::new("pool-percent")
+        .about("show pool used in percent")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .arg(fs)
+        .arg(pool);
+
+    Command::new(crate_name!())
+        .version(crate_version!())
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(build_cache())
+        .subcommand(build_list())
+        .subcommand(pool_percent)
+        .subcommand(build_prometheus())
+}
+
+fn build_cache() -> Command<'static> {
+    let cache_nmon = Command::new("nmon")
+        .about("cache local NSD block devices for use with nmon")
+        .arg(arg_device_cache())
+        .arg(arg_force())
+        .arg(
+            arg_output()
+                .default_value(mmoxi::nmon::DEFAULT_DEVICE_CACHE),
+        )
+        .after_long_help(
+"The local NSD block device association needs to be figured out with \
+ `mmlsnsd -X`, which is an expensive operation. That's why this caching \
+ command exists."
+        );
+
+    let cache_nsds = Command::new("nsds")
+        .about("cache local NSD block device association")
+        .arg(arg_force())
+        .arg(
+            arg_output()
+                .default_value(mmoxi::nsd::DEFAULT_LOCAL_DEVICE_CACHE),
+        )
+        .after_long_help(
+"The local NSD block device association needs to be figured out with \
+ `mmlsnsd -X`, which is an expensive operation. That's why this caching \
+ command exists."
+        );
+
+    Command::new("cache")
+        .about("cache results for later use")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(cache_nmon)
+        .subcommand(cache_nsds)
+}
+
+fn build_list() -> Command<'static> {
+    let list_fs = Command::new("filesystems")
+        .about("list file system names")
+        .alias("fs")
+        .disable_help_flag(true)
+        .disable_version_flag(true);
+
+    Command::new("list")
+        .about("list commands")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(list_fs)
+}
+
+pub fn build_prometheus() -> Command<'static> {
+    let prom_pool_block = Command::new("block")
+        .about("Gather block device metrics grouped by pool.")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .arg(arg_device_cache())
+        .arg(arg_force())
+        .arg(arg_output())
+        .after_long_help("Run locally on every file server.");
+
+    let prom_pool_usage = Command::new("usage")
+        .about("Gather pool usage metrics.")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .arg(arg_output())
+        .after_long_help("Run on cluster manager only.");
+
+    let prom_pool = Command::new("pool")
+        .about("Pool metrics.")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(prom_pool_block)
+        .subcommand(prom_pool_usage);
+
+    let prom_quota = Command::new("quota")
+        .about("Quota metrics.")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .arg(arg_output())
+        .after_long_help("Run every hour on cluster manager only.");
+
+    Command::new("prometheus")
+        .about("prometheus metrics")
+        .alias("prom")
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(prom_pool)
+        .subcommand(prom_quota)
+}
+
+fn arg_device_cache() -> Arg<'static> {
+    Arg::new("device-cache")
+        .long("device-cache")
+        .default_value(mmoxi::nsd::DEFAULT_LOCAL_DEVICE_CACHE)
+        .help("local NSD block device cache")
+        .long_help("Cache for local NSD block device associations.")
+}
+
+fn arg_force() -> Arg<'static> {
+    Arg::new("force")
+        .short('f')
+        .long("force")
+        .help("force cache recreation")
+        .long_help("Force recreating the cache.")
+}
+
+fn arg_output() -> Arg<'static> {
+    Arg::new("output")
+        .short('o')
+        .long("output")
+        .help("output file")
+        .long_help("Output file.")
+}
---
+++ src/bin/mmoxi/main.rs
@@ -0,0 +1,182 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic, clippy::nursery, clippy::cargo)]
+
+use std::fs::File;
+use std::io::{self, BufWriter, Write};
+
+use anyhow::{anyhow, Context, Result};
+use clap::ArgMatches;
+
+mod cli;
+
+fn main() -> Result<()> {
+    let args = cli::args();
+
+    match args.subcommand() {
+        Some(("cache", args)) => dispatch_cache(args),
+        Some(("list", args)) => dispatch_list(args),
+        Some(("pool-percent", args)) => run_pool_percent(args),
+        Some(("prometheus", args)) => dispatch_prom(args),
+
+        _ => Err(anyhow!("subcommand is required")),
+    }
+}
+
+// ----------------------------------------------------------------------------
+// subcommand dispatcher
+// ----------------------------------------------------------------------------
+
+fn dispatch_cache(args: &ArgMatches) -> Result<()> {
+    match args.subcommand() {
+        Some(("nmon", args)) => run_cache_nmon(args),
+        Some(("nsds", args)) => run_cache_nsds(args),
+
+        _ => Err(anyhow!("subcommand is required")),
+    }
+}
+
+fn dispatch_list(args: &ArgMatches) -> Result<()> {
+    match args.subcommand() {
+        Some(("filesystems", _args)) => run_list_filesystems(),
+
+        _ => Err(anyhow!("subcommand is required")),
+    }
+}
+
+fn dispatch_prom(args: &ArgMatches) -> Result<()> {
+    match args.subcommand() {
+        Some(("pool", args)) => dispatch_prom_pool(args),
+        Some(("quota", args)) => run_prom_quota(args),
+
+        _ => Err(anyhow!("subcommand is required")),
+    }
+}
+
+fn dispatch_prom_pool(args: &ArgMatches) -> Result<()> {
+    match args.subcommand() {
+        Some(("block", args)) => run_prom_pool_block(args),
+        Some(("usage", args)) => run_prom_pool_usage(args),
+
+        _ => Err(anyhow!("subcommand is required")),
+    }
+}
+
+// ----------------------------------------------------------------------------
+// runner
+// ----------------------------------------------------------------------------
+
+fn run_cache_nmon(args: &ArgMatches) -> Result<()> {
+    let force = args.is_present("force");
+
+    // UNWRAP has default
+    let device_cache = args.value_of("device-cache").unwrap();
+
+    // UNWRAP has default
+    let output = args.value_of("output").unwrap();
+
+    let output = File::create(output)
+        .with_context(|| format!("creating output file: {}", output))?;
+
+    let mut output = BufWriter::new(output);
+
+    mmoxi::nmon::by_pool_cached(device_cache, force, &mut output)
+}
+
+fn run_cache_nsds(args: &ArgMatches) -> Result<()> {
+    let force = args.is_present("force");
+
+    // UNWRAP has default
+    let output = args.value_of("output").unwrap();
+
+    let _nsds = mmoxi::nsd::local_cached(output, force)?;
+
+    Ok(())
+}
+
+fn run_list_filesystems() -> Result<()> {
+    let names = mmoxi::fs::names()?;
+
+    for name in names {
+        println!("{name}");
+    }
+
+    Ok(())
+}
+
+fn run_pool_percent(args: &ArgMatches) -> Result<()> {
+    let filesystem = args
+        .value_of("filesystem")
+        .context("no filesystem argument")?;
+
+    let filesystem = mmoxi::pool::run(filesystem)?;
+
+    let pool_arg = args.value_of("pool").context("no pool argument")?;
+
+    let pool = filesystem
+        .pools()
+        .iter()
+        .find(|pool| pool.name() == pool_arg)
+        .with_context(|| format!("pool {} not found", pool_arg))?;
+
+    let data_pool_size = pool
+        .data()
+        .with_context(|| format!("pool {} is not object data", pool_arg))?;
+
+    println!("{}", data_pool_size.used_percent());
+
+    Ok(())
+}
+
+fn run_prom_pool_block(args: &ArgMatches) -> Result<()> {
+    let mut output = output_to_bufwriter(args)?;
+
+    // UNWRAP has default
+    let device_cache = args.value_of("device-cache").unwrap();
+
+    let force = args.is_present("force");
+
+    let metrics = mmoxi::prom::pool_block_device_metrics(device_cache, force)?;
+    metrics.to_prom(&mut output)?;
+
+    Ok(())
+}
+
+fn run_prom_pool_usage(args: &ArgMatches) -> Result<()> {
+    let mut output = output_to_bufwriter(args)?;
+
+    let names = mmoxi::fs::names()?;
+
+    let filesystems = mmoxi::pool::run_all(&names)?;
+
+    mmoxi::pool::to_prom(&filesystems, &mut output)
+        .context("converting internal data to prometheus")?;
+
+    Ok(())
+}
+
+fn run_prom_quota(args: &ArgMatches) -> Result<()> {
+    let mut output = output_to_bufwriter(args)?;
+
+    let data = mmoxi::quota::Data::from_reader(io::stdin().lock())?;
+    data.to_prom(&mut output)?;
+
+    Ok(())
+}
+
+// ----------------------------------------------------------------------------
+// helper
+// ----------------------------------------------------------------------------
+
+fn output_to_bufwriter(
+    args: &ArgMatches,
+) -> Result<BufWriter<Box<dyn Write>>> {
+    let output = args.value_of("output");
+
+    let output: Box<dyn Write> = if let Some(output) = output {
+        Box::new(File::create(output)?)
+    } else {
+        Box::new(io::stdout())
+    };
+
+    Ok(BufWriter::new(output))
+}
